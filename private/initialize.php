69661dff312cd56dd74b459b242d88410de72877
Michael Whang <mike@tinybot.ca>
2018-02-19 15:38:02 -0800

    Quick typo fix

--- private/initialize.php
+++ private/initialize.php
@@ -35,6 +35,9 @@
 
     date_default_timezone_set('America/Los_Angeles');
 
+    // Check for SSL, if not already on SSL, redirect to SSL
+    // Source: http://thisinterestsme.com/php-forcing-https-over-http/
+    // Todo: Package into a function within functions.php
     //If the HTTPS is not found to be "on"
     if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on")
     {
