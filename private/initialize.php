49d377945ba44df88a860d8e186dc18b19f49e61
Michael Whang <mike@tinybot.ca>
2018-02-19 11:26:14 -0800

    auto redirect ssl
    redirect user to the SSL version of the page they are on

--- private/initialize.php
+++ private/initialize.php
@@ -34,3 +34,12 @@
     $errors = [];
 
     date_default_timezone_set('America/Los_Angeles');
+
+    //If the HTTPS is not found to be "on"
+    if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on")
+    {
+        //Tell the browser to redirect to the HTTPS URL.
+        header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+        //Prevent the rest of the script from executing.
+        exit;
+    }
