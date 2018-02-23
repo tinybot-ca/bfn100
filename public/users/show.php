f425a86fbdf33d910d4b841fa8e6f5b80a5aaa02
Michael Whang <mike@tinybot.ca>
2018-02-23 11:26:18 -0800

    Cleaned up user profile
    To use new standard Bootstrap row and column layout

--- public/users/show.php
+++ public/users/show.php
@@ -10,33 +10,39 @@
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
 <div class="container">
-    <h3 class="mt-3">User: <?php echo h($user['first_name'] . ' ' . $user['last_name']); ?></h3>
+  <div class="row">
+    <div class="col-md-12">
+      <h3>User Profile</h3>
+      <div class="actions mb-3">
+          <a class="btn btn-primary btn-sm" href="<?php echo url_for('/users/index.php'); ?>">Back to List</a>
+          <a class="btn btn-primary btn-sm" href="<?php echo url_for('/users/edit.php?id=' . h(u($user['id']))); ?>">Edit</a>
+          <a class="btn btn-primary btn-sm" href="<?php echo url_for('/users/delete.php?id=' . h(u($user['id']))); ?>">Delete</a>
+      </div>
 
-    <div class="actions mb-3">
-        <a class="btn btn-primary btn-sm" href="<?php echo url_for('/users/index.php'); ?>">Back to List</a>
-        <a class="btn btn-primary btn-sm" href="<?php echo url_for('/users/edit.php?id=' . h(u($user['id']))); ?>">Edit</a>
-        <a class="btn btn-primary btn-sm" href="<?php echo url_for('/users/delete.php?id=' . h(u($user['id']))); ?>">Delete</a>
-    </div>
+      <!-- User Profile -->
+      <div class="attributes">
+          <dl>
+              <dt>First Name</dt>
+              <dd><?php echo h($user['first_name']); ?></dd>
+          </dl>
+          <dl>
+              <dt>Last Name</dt>
+              <dd><?php echo h($user['last_name']); ?></dd>
+          </dl>
+          <dl>
+              <dt>Email</dt>
+              <dd><?php echo h($user['email']); ?></dd>
+          </dl>
+          <dl>
+              <dt>Username</dt>
+              <dd><?php echo h($user['username']); ?></dd>
+          </dl>
+      </div>
 
-    <div class="attributes">
-        <dl>
-            <dt>First Name</dt>
-            <dd><?php echo h($user['first_name']); ?></dd>
-        </dl>
-        <dl>
-            <dt>Last Name</dt>
-            <dd><?php echo h($user['last_name']); ?></dd>
-        </dl>
-        <dl>
-            <dt>Email</dt>
-            <dd><?php echo h($user['email']); ?></dd>
-        </dl>
-        <dl>
-            <dt>Username</dt>
-            <dd><?php echo h($user['username']); ?></dd>
-        </dl>
-    </div>
+      <!-- Historical Push-up Activity -->
 
+    </div><!-- col-md-12 -->
+  </div><!-- row -->
 </div><!-- container -->
 
 <?php include(SHARED_PATH . '/public_footer.php'); ?>
